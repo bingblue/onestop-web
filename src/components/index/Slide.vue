8deb1191dd2d333b7cd4b91b6b97f1f53bbb35a4
XiaoMuCOOL <gavin@bingblue.com>
2019-10-19 14:28:20 +0800

    add slide

--- src/components/index/Slide.vue
+++ src/components/index/Slide.vue
@@ -1,40 +1,19 @@
 <template>
   <el-row>
     <el-col :span="18" :offset="3" :xs="{span: 22, offset: 1}" class="slide">
-      <a href="javascrip:;" class="pre"><img src="@/assets/index/index_slide_pre.png" alt="pre"></a>
+      <a href="javascrip:;" @click="pre" class="pre"><img src="@/assets/index/index_slide_pre.png" alt="pre"></a>
       <div class="wrap">
-        <ul>
-          <li>
-            <router-link to="/" title="">
-              <img src="@/assets/index/index_slide_1.jpg" alt="商务提包">
-              <span class="name">商务提包</span>
-              <span class="price">￥330</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/" title="">
-              <img src="@/assets/index/index_slide_2.jpg" alt="商务鞋子">
-              <span class="name">商务鞋子</span>
-              <span class="price">￥230</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/" title="">
-              <img src="@/assets/index/index_slide_3.jpg" alt="商务眼镜">
-              <span class="name">商务眼镜</span>
-              <span class="price">￥130</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/" title="">
-              <img src="@/assets/index/index_slide_4.jpg" alt="商务帽子">
-              <span class="name">商务帽子</span>
-              <span class="price">￥30</span>
+        <ul :style="style" ref="ul">
+          <li v-for="(item, index) in slideData" :key="index">
+            <router-link :to="item.url" :title="item.name">
+              <img :src="item.img" :alt="item.name">
+              <span class="name">{{item.name}}</span>
+              <span class="price">￥{{item.price}}</span>
             </router-link>
           </li>
         </ul>
       </div>
-      <a href="javascrip:;" class="next"><img src="@/assets/index/index_slide_next.png" alt="pre"></a>
+      <a href="javascrip:;" @click="next" class="next"><img src="@/assets/index/index_slide_next.png" alt="pre"></a>
     </el-col>
   </el-row>
 </template>
@@ -44,7 +23,55 @@ export default {
   name: 'Slide',
   data () {
     return {
-
+      slideData: [
+        {
+          name: '商务帽子',
+          price: '30',
+          img: '/img/index_slide_4.jpg',
+          url: '#'
+        }, {
+          name: '商务提包',
+          price: '330',
+          img: '/img/index_slide_1.jpg',
+          url: '#'
+        }, {
+          name: '商务鞋子',
+          price: '130',
+          img: '/img/index_slide_2.jpg',
+          url: '#'
+        }, {
+          name: '商务眼睛',
+          price: '230',
+          img: '/img/index_slide_3.jpg',
+          url: '#'
+        }, {
+          name: '商务提包',
+          price: '330',
+          img: '/img/index_slide_1.jpg',
+          url: '#'
+        }
+      ],
+      now: 0,
+      style: {
+        transform: 'translateX(0px)'
+      }
+    }
+  },
+  methods: {
+    pre () {
+      this.now = this.now - 1 < 0 ? this.slideData.length - 4 : this.now - 1
+      this.goTo()
+    },
+    next () {
+      this.now = this.now + 1 > (this.slideData.length - 4) ? 0 : this.now + 1
+      this.goTo()
+    },
+    goTo () {
+      // 获取<li>元素
+      let domLi = this.$refs.ul.children[0]
+      // 计算<li>宽度 + marginRight的值，marginRight为（宽度/7.5），见CSS
+      let domDiff = domLi.offsetWidth + (domLi.offsetWidth / 7.5)
+      this.style.transform = `translateX(-${this.now * domDiff}px)`
     }
   }
 }
@@ -69,10 +96,15 @@ export default {
     overflow hidden
     width 100%
   ul
+    transition 1s
+    width 9999px
     li
       float left
-      width 22.75%
-      margin-right 3%
+      width 15vw
+      margin-right 2vw
+      @media screen and (max-width: 750px)
+        width 19vw
+        margin-right 2vw
       &:last-child
         margin-right 0
       img
